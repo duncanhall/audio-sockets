b26277b10e59c02d39559a415e3e0dfafb650907
Duncan Hall <himself@duncanhall.net>
2014-03-03 23:18:43 +0000

    Added basic slave page

--- server/server.js
+++ server/server.js
@@ -3,17 +3,29 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var slaveID = 0;
 
 server.listen(8000);
 
 app.use("/", express.static(__dirname + "/../client/"));
 
 io.sockets.on('connection', function (socket) {
-
-	socket.emit('msg', { hello: 'world' });
+	
 	socket.on('msg', function(data) {
-		console.log("Received: " + data);
+		
+		if (socket.id != slaveID)
+		{
+			io.sockets.socket(slaveID).emit(data);
+		}
 	});	
+
+	socket.on('id', function(data) {
+		if (data == 'slave')
+		{
+			console.log('Slave detected');
+			slaveID = socket.id;
+		}
+	});
 
 	console.log('Client connected');
 
