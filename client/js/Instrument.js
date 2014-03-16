7d74d46955dc790420ca9878f1fcead0bf7d4d0d
Duncan Hall <himself@duncanhall.net>
2014-03-16 17:27:11 +0000

    Support multiple instrument volumes

--- client/js/Instrument.js
+++ client/js/Instrument.js
@@ -10,24 +10,26 @@ var Instrument = Class.extend({
 		this.synth = synth;
 		this.intro = intro;
 		this.outro = outro;
+		this.volume = 1;
 
 		this.element = document.createElement('div');
 		this.element.className = 'instrument';
-		
 	},
 
 	playIntro: function () {
 
+		Synth.setVolume(this.volume);
 		this.synth.play.apply(this.synth, this.intro.toArgs());
 		this.element.className = 'instrument grow';		
-
+		Synth.setVolume(1);
 	},
 
 	playOutro: function () {
 
+		Synth.setVolume(this.volume);
 		this.synth.play.apply(this.synth, this.outro.toArgs());
 		this.element.className = 'instrument';		
-
+		Synth.setVolume(1);
 	},
 
 	destroy: function () {
@@ -47,12 +49,10 @@ var Instrument = Class.extend({
 */
 var Bass = Instrument.extend({
 
-	init: function (synth) {
+	init: function () {
 
-		this._super(synth,
-			new Note('C', 1, 1), 
-			new Note('C', 2, 1)
-		);
+		this._super(Synth.createInstrument('organ'), 
+			new Note('C', 1, 1), new Note('C', 2, 1));
 	}	
 
 });
@@ -67,12 +67,11 @@ var Mid = Instrument.extend({
 
 	init: function (synth) {
 
-		this._super(synth,
-			new Note('A', 2, 1.4),
-			new Note('G', 2, 1.4)
-			
-		);
-	}	
+		this._super(Synth.createInstrument('acoustic'), 
+			new Note('C', 4, 1.4), new Note('C', 3, 1.6));
+
+		this.volume = 0.1;
+	}
 
 });
 
@@ -90,13 +89,11 @@ var Note = Class.extend({
 		this.note = note;
 		this.octave = octave;
 		this.duration = duration;
-
 	},
 
 	toArgs: function () {
 
 		return [this.note, this.octave, this.duration];
-
 	}
 
 });
