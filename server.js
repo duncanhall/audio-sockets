df0be314c175475766602c11a0be9ab252fdf3a0
Duncan Hall <himself@duncanhall.net>
2014-10-24 13:22:38 +0100

    Add openshift port / ip to server

--- server.js
+++ server.js
@@ -15,9 +15,15 @@ require('dns').lookup(require('os').hostname(), function (err, add, fam) {
         var io = require('socket.io').listen(server);
         var relay = require(path.resolve('backend/io/SocketRelay'));
 
+        var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
         app.use("/", express.static(__dirname + '/frontend/'));
-        server.listen(8000);
-        relay.init(io);
+
+        server.listen(server_port, server_ip_address, function(){
+            console.log("Listening on " + server_ip_address + ", server_port " + port);
+            relay.init(io);
+        });
     }
 });
 
