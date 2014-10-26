3fb6671935cadb0a541373c316329dce4e3c0bf4
Duncan Hall <himself@duncanhall.net>
2014-10-26 16:43:34 +0000

    Refactor server, upgrade express

--- server.js
+++ server.js
@@ -2,148 +2,46 @@
 
 var path = require('path');
 
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+    if (err !== null || add === null) {
+        console.error('Could not detect IP: ' + err);
+    }
+    else {
 
-    //  Scope.
-    var self = this;
+        var PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        var IP_ADDR = process.env.OPENSHIFT_NODEJS_IP || add;
 
+        var express = require('express');
+        var app = express();
+        var server = require('http').createServer(app);
+        var io = require('socket.io').listen(server);
+        var relay = require(path.resolve('backend/io/SocketRelay'));
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+        app.use("/", express.static(__dirname + '/frontend/'));
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        }
-    };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-            console.log('%s: Received %s - terminating sample app ...',
-                Date(Date.now()), sig);
-            process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-            'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-                process.on(element, function() { self.terminator(element); });
-            });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(200);
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-
-
-
-        require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-
-            if (err !== null || add === null) {
-                console.error('Could not detect IP: ' + err);
-            }
-            else {
-                var SERVER_ADDRESS = add;
-
-                var express = require('express');
-                var app = express();
-                var server = require('http').createServer(app);
-                var io = require('socket.io').listen(server);
-                var relay = require(path.resolve('backend/io/SocketRelay'));
-
-                var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-                var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || SERVER_ADDRESS;
-
-                app.use("/", express.static(__dirname + '/frontend/'));
-
-                for (var r in self.routes) {
-                    app.get(r, self.routes[r]);
-                }
-
-                server.listen(server_port, server_ip_address, function(){
-                    console.log("Listening on " + server_ip_address + ", server_port " + server_port);
-                    relay.init(io);
-                });
-            }
+        server.listen(PORT, IP_ADDR, function () {
+            console.log("Listening on " + IP_ADDR + ", server_port " + PORT);
+            relay.init(io);
         });
 
+    }
+});
 
 
 
-    };
+function exit(sig){
+    if (typeof sig === "string") {
+        console.log('%s: Received %s - terminating sample app ...', Date(Date.now()), sig);
+        process.exit(1);
+    }
+    console.log('%s: Node server stopped.', Date(Date.now()));
+}
+
+['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT','SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM']
+.forEach(function(element, index, array) {
+    process.on(element, function() { exit(element); });
+});
 
 
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.setupTerminationHandlers();
-        self.initializeServer();
-    };
-
-
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-
+process.on('exit', function() { exit(); });
